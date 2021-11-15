c072afc129970e0aa31e033f165935fee4c37e3f
Nathan Yee <nyee@csu.fullerton.edu>
2021-11-15 11:23:40 -0800

    converted styling to use flexbox. working on image alignment

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 import Header from './components/Header';
 import Posts from './components/Posts';
 import Postdata from './components/Postdata';
+import './styles.css';
 
 function App() {
   return (
     <div>
       <Header />
-      {Postdata.map(data => (
-        <Posts 
-          alt={data.alt}
-          image={data.image}
-          link={data.link}
-          key={data.key}
-        />
-      ))}
+      <div className="container">
+        {Postdata.map(data => (
+          <Posts 
+            alt={data.alt}
+            image={data.image}
+            link={data.link}
+            key={data.key}
+          />
+        ))}
+      </div>
     </div>
   );
 }
