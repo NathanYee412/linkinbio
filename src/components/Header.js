fee4c24ec83805fa98d39ef6b5dab6fe15314dee
Nathan Yee <nyee@csu.fullerton.edu>
2021-11-15 11:24:15 -0800

    converted to flexbox

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return(
         <div>
             <img className="logo" src="./img/LSO_flat_logo.png" alt="logo" />
-            <button className="pageButton" href="https://lso-inc.com/">Link to Page</button>
+            <button className="pageButton" href="https://lso-inc.com/">Visit our Website!</button>
         </div>
     );
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Posts(props) {
     return(
-        <div className="post">
+        <div className="item">
             <a href={props.link}>
                 <img src={props.image} alt={props.alt}></img>
             </a>
