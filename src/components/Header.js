6f6b0b699df4c5824bfc7d55078a2d7cc42d7441
Nathan Yee <nyee@csu.fullerton.edu>
2021-11-18 13:30:34 -0800

    styling and button changes. Added design to top of page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Header() {
     return(
         <div>
+            <img className="banner" src="./img/banner.png" alt="banner" />
             <img className="logo" src="./img/LSO_flat_logo.png" alt="logo" />
             <a href="https://lso-inc.com/"><button className="pageButton">Visit our Website!</button></a>
         </div>
