34d34add6ce80b3c27e40eff955c6a5a6ce2e5cd
Nathan Yee <nyee@csu.fullerton.edu>
2021-11-18 11:10:05 -0800

    Updated posts to current instagram state

--- src/components/Postdata.js
+++ src/components/Postdata.js
@@ -1,9 +1,27 @@
 const posts = [
     {
+        alt: "EO Sterilization",
+        link:"https://lso-inc.com/blog/4-best-practices-for-better-batch-eo-sterilization/",
+        image:"https://media-exp1.licdn.com/dms/image/C5622AQGLUGNgIn6-kw/feedshare-shrink_800/0/1637255701445?e=1640217600&v=beta&t=udanmPHpecsB6kO_Egsjg461qu4-qh4tSfdNI3rdRIs",
+        key: 6
+    },
+    {
+        alt: "4 Medical Device Packaging Pitfalls to Avoid",
+        link:"https://lso-inc.com/blog/4-medical-device-packaging-pitfalls-to-avoid/",
+        image:"https://f.hubspotusercontent20.net/hubfs/14497222/social-suggested-images/Instagram%204%20Medical%20Device%20Packaging%20Pitfalls%20to%20Avoid.png",
+        key: 5
+    },
+    {
+        alt: "Navigating 510k clearance",
+        link:"https://lso-inc.com/blog/navigating-510k-approval-4-key-steps-in-the-manufacturing-process/",
+        image:"https://f.hubspotusercontent20.net/hubfs/14497222/social-suggested-images/Navigating%20510k%20Clearance%204%20Key%20Steps%20in%20the%20Manufacturing%20Process.png",
+        key: 4
+    },
+    {
         alt: "Veterans Day",
-        link:"https://www.linkedin.com/company/lso-inc",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQGLm6Bw-AVpVA/feedshare-shrink_800/0/1636657622626?e=1639612800&v=beta&t=2bfC9k_-pcut1gPi8l4uNNdMsm_wYPZoM5uxOajnBTg",
-        key: 1
+        link:"https://www.linkedin.com/feed/update/urn:li:activity:6864639636262793216",
+        image:"https://media-exp1.licdn.com/dms/image/C5622AQGLm6Bw-AVpVA/feedshare-shrink_800/0/1636657622626?e=1640217600&v=beta&t=t57gU7tYuFvrB-VTrRXJqnA9t8D0_kgFW0XQyXdCfdo",
+        key: 3
     },
     {
         alt: "ISO 11607",
@@ -15,109 +33,13 @@ const posts = [
         alt: "medical device sterilization",
         link:"https://lso-inc.com/blog/3-often-overlooked-elements-of-medical-device-sterilization/",
         image:"https://media-exp1.licdn.com/dms/image/C5622AQFKpzftQfNtcw/feedshare-shrink_1280/0/1636568942104?e=1639612800&v=beta&t=4FGi62wfLMpjhLDpT_xxeywWd9QA33dwMgnrGnoqML8",
-        key: 3
-    },
-    {
-        alt: "3 Keys to More Efficient Distribution Simulation Testing",
-        link:"https://lso-inc.com/blog/3-keys-to-more-efficient-distribution-simulation-testing/",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQHgUH4Y5I7AKw/feedshare-shrink_1280/0/1635271793676?e=1639612800&v=beta&t=zcF6DgsK6x9Pns_k8GruZQEcZ2flRQQHhLBGZ5pjxrU",
-        key: 4
-    }, // test post items
-    {
-        alt: "Veterans Day",
-        link:"https://www.linkedin.com/company/lso-inc",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQGLm6Bw-AVpVA/feedshare-shrink_800/0/1636657622626?e=1639612800&v=beta&t=2bfC9k_-pcut1gPi8l4uNNdMsm_wYPZoM5uxOajnBTg",
         key: 1
     },
     {
-        alt: "ISO 11607",
-        link:"https://lso-inc.com/blog/iso-11607-answering-manufacturers-most-common-questions/",
-        image:"https://lso-inc.com/wp-content/uploads/2021/09/11607_blog_feat-scaled.jpg",
-        key: 2
-    },
-    {
-        alt: "medical device sterilization",
-        link:"https://lso-inc.com/blog/3-often-overlooked-elements-of-medical-device-sterilization/",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQFKpzftQfNtcw/feedshare-shrink_1280/0/1636568942104?e=1639612800&v=beta&t=4FGi62wfLMpjhLDpT_xxeywWd9QA33dwMgnrGnoqML8",
-        key: 3
-    },
-    {
         alt: "3 Keys to More Efficient Distribution Simulation Testing",
         link:"https://lso-inc.com/blog/3-keys-to-more-efficient-distribution-simulation-testing/",
         image:"https://media-exp1.licdn.com/dms/image/C5622AQHgUH4Y5I7AKw/feedshare-shrink_1280/0/1635271793676?e=1639612800&v=beta&t=zcF6DgsK6x9Pns_k8GruZQEcZ2flRQQHhLBGZ5pjxrU",
-        key: 4
-    },
-    {
-        alt: "Veterans Day",
-        link:"https://www.linkedin.com/company/lso-inc",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQGLm6Bw-AVpVA/feedshare-shrink_800/0/1636657622626?e=1639612800&v=beta&t=2bfC9k_-pcut1gPi8l4uNNdMsm_wYPZoM5uxOajnBTg",
-        key: 1
-    },
-    {
-        alt: "ISO 11607",
-        link:"https://lso-inc.com/blog/iso-11607-answering-manufacturers-most-common-questions/",
-        image:"https://lso-inc.com/wp-content/uploads/2021/09/11607_blog_feat-scaled.jpg",
-        key: 2
-    },
-    {
-        alt: "medical device sterilization",
-        link:"https://lso-inc.com/blog/3-often-overlooked-elements-of-medical-device-sterilization/",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQFKpzftQfNtcw/feedshare-shrink_1280/0/1636568942104?e=1639612800&v=beta&t=4FGi62wfLMpjhLDpT_xxeywWd9QA33dwMgnrGnoqML8",
-        key: 3
-    },
-    {
-        alt: "3 Keys to More Efficient Distribution Simulation Testing",
-        link:"https://lso-inc.com/blog/3-keys-to-more-efficient-distribution-simulation-testing/",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQHgUH4Y5I7AKw/feedshare-shrink_1280/0/1635271793676?e=1639612800&v=beta&t=zcF6DgsK6x9Pns_k8GruZQEcZ2flRQQHhLBGZ5pjxrU",
-        key: 4
-    },
-    {
-        alt: "Veterans Day",
-        link:"https://www.linkedin.com/company/lso-inc",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQGLm6Bw-AVpVA/feedshare-shrink_800/0/1636657622626?e=1639612800&v=beta&t=2bfC9k_-pcut1gPi8l4uNNdMsm_wYPZoM5uxOajnBTg",
-        key: 1
-    },
-    {
-        alt: "ISO 11607",
-        link:"https://lso-inc.com/blog/iso-11607-answering-manufacturers-most-common-questions/",
-        image:"https://lso-inc.com/wp-content/uploads/2021/09/11607_blog_feat-scaled.jpg",
-        key: 2
-    },
-    {
-        alt: "medical device sterilization",
-        link:"https://lso-inc.com/blog/3-often-overlooked-elements-of-medical-device-sterilization/",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQFKpzftQfNtcw/feedshare-shrink_1280/0/1636568942104?e=1639612800&v=beta&t=4FGi62wfLMpjhLDpT_xxeywWd9QA33dwMgnrGnoqML8",
-        key: 3
-    },
-    {
-        alt: "3 Keys to More Efficient Distribution Simulation Testing",
-        link:"https://lso-inc.com/blog/3-keys-to-more-efficient-distribution-simulation-testing/",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQHgUH4Y5I7AKw/feedshare-shrink_1280/0/1635271793676?e=1639612800&v=beta&t=zcF6DgsK6x9Pns_k8GruZQEcZ2flRQQHhLBGZ5pjxrU",
-        key: 4
-    },
-    {
-        alt: "Veterans Day",
-        link:"https://www.linkedin.com/company/lso-inc",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQGLm6Bw-AVpVA/feedshare-shrink_800/0/1636657622626?e=1639612800&v=beta&t=2bfC9k_-pcut1gPi8l4uNNdMsm_wYPZoM5uxOajnBTg",
-        key: 1
-    },
-    {
-        alt: "ISO 11607",
-        link:"https://lso-inc.com/blog/iso-11607-answering-manufacturers-most-common-questions/",
-        image:"https://lso-inc.com/wp-content/uploads/2021/09/11607_blog_feat-scaled.jpg",
-        key: 2
-    },
-    {
-        alt: "medical device sterilization",
-        link:"https://lso-inc.com/blog/3-often-overlooked-elements-of-medical-device-sterilization/",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQFKpzftQfNtcw/feedshare-shrink_1280/0/1636568942104?e=1639612800&v=beta&t=4FGi62wfLMpjhLDpT_xxeywWd9QA33dwMgnrGnoqML8",
-        key: 3
-    },
-    {
-        alt: "3 Keys to More Efficient Distribution Simulation Testing",
-        link:"https://lso-inc.com/blog/3-keys-to-more-efficient-distribution-simulation-testing/",
-        image:"https://media-exp1.licdn.com/dms/image/C5622AQHgUH4Y5I7AKw/feedshare-shrink_1280/0/1635271793676?e=1639612800&v=beta&t=zcF6DgsK6x9Pns_k8GruZQEcZ2flRQQHhLBGZ5pjxrU",
-        key: 4
+        key: 0
     },
     
 ]
